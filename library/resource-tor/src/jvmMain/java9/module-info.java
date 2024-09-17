94ce36da6dd59bc7094a3f6abfdab3c69fdf2d0d
Matthew Nelson <developer@matthewnelson.io>
2024-09-17 18:52:25 -0400

    Delete now obsoleted modules

--- library/resource-tor/src/jvmMain/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module io.matthewnelson.kmp.tor.resource.tor {
-    requires transitive io.matthewnelson.kmp.tor.core.api;
-    requires io.matthewnelson.kmp.tor.core.resource;
-
-    exports io.matthewnelson.kmp.tor.resource.tor;
-}
