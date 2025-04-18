d30c386b3ba133fcbcaa39a50e2dc475c3fd9d6a
Matthew Nelson <developer@matthewnelson.io>
2025-04-18 09:44:40 -0400

    Fix `JPMS` module definitions for reflection based access to loaders (#115)

--- library/resource-geoip/src/jvmMain/java9/module-info.java
+++ library/resource-geoip/src/jvmMain/java9/module-info.java
@@ -2,5 +2,6 @@ module io.matthewnelson.kmp.tor.resource.geoip {
     requires io.matthewnelson.kmp.tor.common.api;
     requires io.matthewnelson.kmp.tor.common.core;
 
+    opens io.matthewnelson.kmp.tor.resource.geoip to io.matthewnelson.kmp.tor.common.core;
     exports io.matthewnelson.kmp.tor.resource.geoip;
 }
