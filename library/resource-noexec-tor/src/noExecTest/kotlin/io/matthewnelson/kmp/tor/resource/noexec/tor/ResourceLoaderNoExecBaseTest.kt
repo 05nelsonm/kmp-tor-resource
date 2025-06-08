6d010a4f78a44a7521d282050f9807edfa38345e
Matthew Nelson <developer@matthewnelson.io>
2025-06-08 07:02:42 -0400

    Extract out test body to helper class

--- library/resource-noexec-tor/src/noExecTest/kotlin/io/matthewnelson/kmp/tor/resource/noexec/tor/ResourceLoaderNoExecBaseTest.kt
+++ library/resource-noexec-tor/src/noExecTest/kotlin/io/matthewnelson/kmp/tor/resource/noexec/tor/ResourceLoaderNoExecBaseTest.kt
@@ -19,6 +19,7 @@ import io.matthewnelson.kmp.file.path
 import io.matthewnelson.kmp.file.readBytes
 import io.matthewnelson.kmp.file.readUtf8
 import io.matthewnelson.kmp.file.resolve
+import io.matthewnelson.kmp.tor.common.api.GeoipFiles
 import io.matthewnelson.kmp.tor.common.api.InternalKmpTorApi
 import io.matthewnelson.kmp.tor.common.api.TorApi
 import io.matthewnelson.kmp.tor.resource.noexec.tor.TestRuntimeBinder.LOADER
@@ -27,9 +28,10 @@ import io.matthewnelson.kmp.tor.resource.noexec.tor.TestRuntimeBinder.WORK_DIR
 import io.matthewnelson.kmp.tor.resource.noexec.tor.internal.RESOURCE_CONFIG_GEOIPS
 import io.matthewnelson.kmp.tor.resource.noexec.tor.internal.RESOURCE_CONFIG_LIB_TOR
 import kotlinx.coroutines.*
+import kotlinx.coroutines.test.TestResult
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import kotlin.test.*
-import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 abstract class ResourceLoaderNoExecBaseTest protected constructor(
@@ -121,27 +123,100 @@ abstract class ResourceLoaderNoExecBaseTest protected constructor(
 
     @Test
     @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
-    fun givenHandle_whenTerminateAndAwait_thenTorExits() = runTest(timeout = 5.minutes) {
-        if (skipTorRunMain) return@runTest
+    fun givenHandle_whenTerminateAndAwait_thenTorExits(): TestResult {
+        val count = runTorMainCount / 25
 
-        val geoipFiles = LOADER.extract().let { files ->
-            if (files.geoip.readBytes().size < 50_000) {
-                // Mock resources...
+        return runTest(timeout = (count * 4).seconds) {
+            if (skipTorRunMain) return@runTest
+
+            val helper = TorApiRunHelper.getOrNull(scope = this)
+            if (helper == null) {
                 println("Skipping...")
                 return@runTest
             }
 
-            files
-        }
+            val hsDir = LOADER.resourceDir.resolve("hs")
 
-        val job = currentCoroutineContext().job
+            fun deleteHsDir() {
+                hsDir.resolve("authorized_clients").delete()
+                hsDir.resolve("hostname").delete()
+                hsDir.resolve("hs_ed25519_public_key").delete()
+                hsDir.resolve("hs_ed25519_secret_key").delete()
+                hsDir.delete()
+            }
+
+            helper.job.invokeOnCompletion { deleteHsDir() }
+
+            helper.args.apply {
+                add("--HiddenServiceDir"); add(hsDir.path)
+                add("--HiddenServiceVersion"); add("3")
+                add("--HiddenServicePort"); add("80")
+            }
+
+            repeat(count) { index ->
+                if (index < 4 || (index + 1) % 5 == 0) {
+                    println("RUN_TOR[${index + 1}]")
+                }
+
+                val api = LOADER.withApi(TestRuntimeBinder) { this }
+                api.torRunMain(helper.args)
+                val completion = helper.job.invokeOnCompletion { api.terminateAndAwaitResult() }
+
+                withContext(helper.bgDispatcher) {
+                    delay(1.seconds)
+                    assertEquals(0, api.terminateAndAwaitResult())
+                }
+
+                completion.dispose()
+
+                val logText = helper.logFile.readUtf8()
+
+                helper.logFile.delete()
+                helper.deleteCacheDir()
+                helper.deleteCacheDir()
+                deleteHsDir()
+
+                listOf(
+                    "Tor can't help you if you use it wrong!",
+                    "Delaying directory fetches: DisableNetwork is set.",
+                    "Owning controller connection has closed -- exiting now.",
+                    "Catching signal TERM, exiting cleanly.",
+                ).mapNotNull { expected ->
+                    if (logText.contains(expected)) return@mapNotNull null
+
+                    "Logs did not contain EXPECTED[$expected]. RUN_TOR[${index + 1}]"
+                }.takeIf { it.isNotEmpty() }?.joinToString(separator = "\n")?.let { errors ->
+                    throw AssertionError(errors + "\n\n" + logText)
+                }
+            }
+        }
+    }
+
+    private class TorApiRunHelper private constructor(val job: Job, val geoipFiles: GeoipFiles) {
 
         val logFile = LOADER.resourceDir.resolve("test.log")
-        job.invokeOnCompletion { logFile.delete() }
-
         val cacheDir = LOADER.resourceDir.resolve("cache")
         val dataDir = LOADER.resourceDir.resolve("data")
-        val hsDir = LOADER.resourceDir.resolve("hs")
+
+        @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+        val bgDispatcher: CoroutineDispatcher = newSingleThreadContext("bg-tor-dispatcher")
+
+        val args = ArrayList<String>(25).apply {
+            add("--ignore-missing-torrc")
+            add("--quiet")
+
+            add("--CacheDirectory"); add(cacheDir.path)
+            add("--DataDirectory"); add(dataDir.path)
+
+            add("--GeoIPFile"); add(geoipFiles.geoip.path)
+            add("--GeoIPv6File"); add(geoipFiles.geoip6.path)
+
+            add("--Log"); add("notice file $logFile")
+            add("--TruncateLogFile"); add("1")
+
+            add("--DormantCanceledByStartup"); add("1")
+            add("--RunAsDaemon"); add("0")
+        }
 
         fun deleteCacheDir() {
             cacheDir.resolve("cached-certs").delete()
@@ -150,91 +225,42 @@ abstract class ResourceLoaderNoExecBaseTest protected constructor(
             cacheDir.resolve("cached-microdescs.new").delete()
             cacheDir.delete()
         }
+
         fun deleteDataDir() {
             dataDir.resolve("lock").delete()
             dataDir.resolve("state").delete()
             dataDir.resolve("keys").delete()
             dataDir.delete()
         }
-        fun deleteHsDir() {
-            hsDir.resolve("authorized_clients").delete()
-            hsDir.resolve("hostname").delete()
-            hsDir.resolve("hs_ed25519_public_key").delete()
-            hsDir.resolve("hs_ed25519_secret_key").delete()
-            hsDir.delete()
-        }
 
-        job.invokeOnCompletion { deleteCacheDir() }
-        job.invokeOnCompletion { deleteDataDir() }
-        job.invokeOnCompletion { deleteHsDir() }
-
-        val args = ArrayList<String>(10).apply {
-            add("--ignore-missing-torrc")
-            add("--quiet")
-
-            add("--DataDirectory"); add(dataDir.path)
-            add("--CacheDirectory"); add(cacheDir.path)
-
-            add("--GeoIPFile"); add(geoipFiles.geoip.toString())
-            add("--GeoIPv6File"); add(geoipFiles.geoip6.toString())
-
-            add("--Log"); add("notice file $logFile")
-            add("--TruncateLogFile"); add("1")
-
-            add("--DormantCanceledByStartup"); add("1")
-            add("--SocksPort"); add("0")
-
-            add("--DisableNetwork"); add("1")
-            add("--RunAsDaemon"); add("0")
-
-            add("--HiddenServiceDir"); add(hsDir.path)
-            add("--HiddenServiceVersion"); add("3")
-            add("--HiddenServicePort"); add("80")
-        }
-
-        val bgDispatcher = newSingleThreadContext("bg-tor-terminate")
-        job.invokeOnCompletion { bgDispatcher.close() }
-
-        repeat(runTorMainCount / 25) { index ->
-            if (index < 4 || (index + 1) % 5 == 0) {
-                println("RUN_TOR[${index + 1}]")
-            }
-
+        init {
             cacheDir.mkdirs()
             dataDir.mkdirs()
 
-            val api = LOADER.withApi(TestRuntimeBinder) {
-                torRunMain(args)
-                this
-            }
+            job.invokeOnCompletion { logFile.delete() }
+            job.invokeOnCompletion { deleteCacheDir() }
+            job.invokeOnCompletion { deleteDataDir() }
+            @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+            job.invokeOnCompletion { (bgDispatcher as CloseableCoroutineDispatcher).close() }
+        }
 
-            val completion = job.invokeOnCompletion { api.terminateAndAwaitResult() }
+        companion object {
 
-            withContext(bgDispatcher) {
-                delay(1.seconds)
-                assertEquals(0, api.terminateAndAwaitResult())
-            }
+            fun getOrNull(
+                scope: TestScope,
+                socksPort: String = "0",
+                disableNetwork: String = "1"
+            ): TorApiRunHelper? {
+                if (!CAN_RUN_FULL_TESTS) return null
 
-            completion.dispose()
-
-            val logText = logFile.readUtf8()
-
-            logFile.delete()
-            deleteCacheDir()
-            deleteCacheDir()
-            deleteHsDir()
-
-            listOf(
-                "Tor can't help you if you use it wrong!",
-                "Delaying directory fetches: DisableNetwork is set.",
-                "Owning controller connection has closed -- exiting now.",
-                "Catching signal TERM, exiting cleanly.",
-            ).mapNotNull { expected ->
-                if (logText.contains(expected)) return@mapNotNull null
-
-                "Logs did not contain EXPECTED[$expected]. RUN_TOR[${index + 1}]"
-            }.takeIf { it.isNotEmpty() }?.joinToString(separator = "\n")?.let { errors ->
-                throw AssertionError(errors + "\n\n" + logText)
+                val geoipFiles = LOADER.extract()
+                if (geoipFiles.geoip.readBytes().size < 50_000) return null
+                val h = TorApiRunHelper(scope.coroutineContext.job, geoipFiles)
+                h.args.apply {
+                    add("--SocksPort"); add(socksPort)
+                    add("--DisableNetwork"); add(disableNetwork)
+                }
+                return h
             }
         }
     }
