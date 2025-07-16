55b930a5186700aff72aaa59f4b5a2483e234213
Matthew Nelson <developer@matthewnelson.io>
2025-07-15 21:48:10 -0400

    Disable JVM tests

--- library/resource-noexec-tor/src/jvmTest/kotlin/io/matthewnelson/kmp/tor/resource/noexec/tor/ResourceLoaderNoExecJvmUnitTest.kt
+++ library/resource-noexec-tor/src/jvmTest/kotlin/io/matthewnelson/kmp/tor/resource/noexec/tor/ResourceLoaderNoExecJvmUnitTest.kt
@@ -21,67 +21,20 @@ import io.ktor.client.engine.HttpClientEngineFactory
 import io.matthewnelson.kmp.tor.common.api.InternalKmpTorApi
 import io.matthewnelson.kmp.tor.common.core.OSHost
 import io.matthewnelson.kmp.tor.common.core.OSInfo
-import kotlinx.coroutines.test.TestResult
-import org.junit.AfterClass
-import org.junit.BeforeClass
-import kotlin.system.exitProcess
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 @OptIn(InternalKmpTorApi::class)
 class ResourceLoaderNoExecJvmUnitTest: ResourceLoaderNoExecBaseTest(
     runTorMainCount = when (OSInfo.INSTANCE.osHost) {
-        is OSHost.Windows -> RUN_TOR_MAIN_COUNT_WINDOWS
-        else -> RUN_TOR_MAIN_COUNT_UNIX
+        is OSHost.Windows -> 0
+        else -> 0
     },
 ) {
-
-    companion object {
-
-        // Disgusting, but Java TLS is absolute trash
-        private var SUCCESSES = 0
-
-        @JvmStatic
-        @BeforeClass
-        fun beforeClass() { SUCCESSES = 0 }
-
-        @JvmStatic
-        @AfterClass
-        fun afterClass() {
-            if (SUCCESSES == 5) exitProcess(0)
-        }
-    }
 
     override val factory: HttpClientEngineFactory<*>? = null
 
     @Test
-    override fun givenResourceLoaderNoExec_whenExtractGeoipFiles_thenIsSuccessful() {
-        super.givenResourceLoaderNoExec_whenExtractGeoipFiles_thenIsSuccessful()
-        SUCCESSES++
-    }
-
-    @Test
-    override fun givenResourceLoaderNoExec_whenWithApi_thenLoadsSuccessfully() {
-        super.givenResourceLoaderNoExec_whenWithApi_thenLoadsSuccessfully()
-        SUCCESSES++
-    }
-
-    @Test
-    override fun givenResourceLoaderNoExec_whenMultipleRuns_thenLibTorIsUnloaded() {
-        super.givenResourceLoaderNoExec_whenMultipleRuns_thenLibTorIsUnloaded()
-        SUCCESSES++
-    }
-
-    @Test
-    override fun givenHandle_whenTerminateAndAwait_thenTorExits(): TestResult {
-        val ret = super.givenHandle_whenTerminateAndAwait_thenTorExits()
-        SUCCESSES++
-        return ret
-    }
-
-    @Test
-    override fun givenTor_whenQueryCheckTorProject_thenConnectionIsUsingTor(): TestResult {
-        val ret = super.givenTor_whenQueryCheckTorProject_thenConnectionIsUsingTor()
-        SUCCESSES++
-        return ret
-    }
+    @Ignore
+    fun stub() {}
 }
