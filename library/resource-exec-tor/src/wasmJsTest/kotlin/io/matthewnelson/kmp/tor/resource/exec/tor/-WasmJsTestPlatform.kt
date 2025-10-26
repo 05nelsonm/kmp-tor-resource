8a090dfd019a53b95ef3f588cc094e58014b5af1
Matthew Nelson <developer@matthewnelson.io>
2025-10-26 06:33:43 -0400

    Update kmp-tor-common: 2.4.0 -> 2.4.1

--- library/resource-exec-tor/src/wasmJsTest/kotlin/io/matthewnelson/kmp/tor/resource/exec/tor/-WasmJsTestPlatform.kt
+++ library/resource-exec-tor/src/wasmJsTest/kotlin/io/matthewnelson/kmp/tor/resource/exec/tor/-WasmJsTestPlatform.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-@file:Suppress("PropertyName")
+@file:Suppress("PropertyName", "OPT_IN_USAGE")
 
 package io.matthewnelson.kmp.tor.resource.exec.tor
 
